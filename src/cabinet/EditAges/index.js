9d7aa3fe71e2a2d91938b01f25ce2bede8bf0f91
roman <ge52@mail.ru>
2017-06-13 22:46:07 +0300

    fix EditAges

--- src/cabinet/EditAges/index.js
+++ src/cabinet/EditAges/index.js
@@ -1,6 +1,40 @@
-stdin: SyntaxError: Unexpected token, expected ; (1:33)
-> 1 | stdin: SyntaxError: Unterminated string constant (1:18)
-    |                                  ^
-  2 | > 1 | import React from "reactages
-  3 |     |                   ^
-  4 |   2 | import { connect } from "react-redux";
+import React from "react";
+import { connect } from "react-redux";
+import TableView from "../../components/TableView";
+import { getAges } from "../../reducers/ages";
+import AddAgeForm from "./form";
+import ButtonBlock from "../../components/ButtonOptions";
+
+const TableRow = props => {
+  const { elm } = props;
+  return (
+    <tr key={elm.id}>
+      <td className="id">{elm.id}</td>
+      <td>{elm.min_age}</td>
+      <td>{elm.max_age}</td>
+      {/* <ButtonBlock onRemoveClick={onRemove([elm.id])} /> */}
+    </tr>
+  );
+};
+
+const EditAges = props => {
+  const rows = props.items.map(elm => <TableRow elm={elm} key={elm.id} />);
+
+  return (
+    <TableView
+      title="Управление возрастными категориями"
+      createBtnLabel="Создать возрастную категориюо"
+      headRow={["#", "Минимальный возраст", "Максимальный возраст"]}
+      rows={rows}
+      specialForm={AddAgeForm}
+    />
+  );
+};
+
+function mapStateToProps(state) {
+  return {
+    items: getAges(state)
+  };
+}
+
+export default connect(mapStateToProps)(EditAges);
